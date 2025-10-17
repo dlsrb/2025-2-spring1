4f9e11f49795c1e6f7fdb412badff4a764ce3402
dlsrb <dlsrb1997@naver.com>
2025-10-17 10:59:53 +0900

    test

---
+++ src/main/java/com/sg/ai/HelloController.java
@@ -0,0 +1,17 @@
+// /sbb/src/main/java/com/sg/ai/HelloController.java
+
+package com.sg.ai;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "안녕하세요 이것은  스프링으로 만든 페이지 입니다";
+    }
+}	
---
+++ src/test/java/com/sg/ai/Sbb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
