4675603f88493013fcefb903a4e25a1c82d590bb
dlsrb <dlsrb1997@naver.com>
2025-11-07 10:44:25 +0900

    이용희 교수님 코드

--- 2025-2-spring1-master/src/main/java/com/sg/ai/answer/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sg.ai.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer>{
-
-}
--- 2025-2-spring1-master/src/main/java/com/sg/ai/question/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sg.ai.question;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
-}
